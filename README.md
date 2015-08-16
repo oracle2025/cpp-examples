1886dfec7b498204e6d0147aaebc13d636ef4ad7
Richard Spindler <richard.spindler@gmail.com>
2015-08-16 15:13:49 +0200

    readme and slightly more complex example for simpleserver

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 #CPP Examples
+
+##simpleserver
+
+Easily run a TCP Server based on boost asio like this:
+
+```cpp
+TCPServer server(portno, [](){return "Hello World\n";}); server.run();
+```
+
--- simpleserver/simpleserver.cpp
+++ simpleserver/simpleserver.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "tcpserver.h"
+#include <sstream>
 
 
 int main(int argc, const char *argv[])
 {
+	int count = 0;
 	try {
-		TCPServer server(2014, [](){return "bla\n";});
+		TCPServer server(2014, [&count](){std::stringstream h; h << count++ << "\n";return h.str();});
 		server.run();
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
