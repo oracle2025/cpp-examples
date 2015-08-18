c2cada0ea862257272d322966b400c0fde5af171
Richard Spindler <rs@eonyk.com>
2015-08-18 13:21:28 +0200

    todolist fltk example app

---
+++ todolist/todolist.cpp
@@ -0,0 +1,164 @@
+/*
+ *
+ * +----------------------------+
+ * |[Add Entry                 ]|
+ * |+--------------------------+|
+ * ||[ ] Get Milk              ||
+ * ||[X] Write Letter          ||
+ * ||                          ||
+ * ||                          ||
+ * |+--------------------------+|
+ * |[Edit Entry  ][Delete Entry]|
+ * +----------------------------+
+ *
+ *
+ * struct TodoItem {
+ * 	 mString m_text;
+ * 	 bool    m_checked;
+ * };
+ *
+ *
+ * struct TodoList {
+ * 	 std::list<TodoItem> m_items;
+ * 	 int add();
+ * 	 void remove(int);
+ * };
+ *
+ *
+ *
+ *
+ */
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Hold_Browser.H>
+#include <FL/fl_ask.H>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+Fl_Browser *g_list_view;
+Fl_Button *g_edit_button;
+Fl_Button *g_delete_button;
+
+void add_cb(Fl_Widget*, void*)
+{
+	const char *result;
+	result = fl_input("Add Entry:", "");
+	if (!result) {
+		return;
+	}
+	if (std::string(result).empty()) {
+		return;
+	}
+
+	g_list_view->add(result);
+}
+
+void edit_cb(Fl_Widget*, void*)
+{
+	int line = g_list_view->value();
+	if (!line) {
+		return;
+	}
+	const char *result;
+	result = fl_input("Edit Entry:", g_list_view->text(line));
+	if (!result) {
+		return;
+	}
+	if (std::string(result).empty()) {
+		return;
+	}
+
+	g_list_view->text(line, result);
+}
+void list_view_cb(Fl_Widget*, void*);
+void delete_cb(Fl_Widget*, void*)
+{
+	int line = g_list_view->value();
+	if (!line) {
+		return;
+	}
+	if (fl_choice("Delete Entry: \n%s", "No, Cancel", "Yes, Delete!", 0, g_list_view->text(line))) {
+		g_list_view->remove(line);
+		list_view_cb(g_list_view, 0);
+	}
+}
+void list_view_cb(Fl_Widget*, void*)
+{
+	if (g_list_view->value()) {
+		g_edit_button->activate();
+		g_delete_button->activate();
+	} else {
+		g_edit_button->deactivate();
+		g_delete_button->deactivate();
+	}
+}
+void window_cb(Fl_Widget* w, void*)
+{
+	{
+		std::ofstream todofile;
+		todofile.open(std::string(getenv("HOME"))+"/.todolist");
+		for (int i = 1; i <= g_list_view->size(); i++) {
+			todofile << g_list_view->text(i) << std::endl;
+		}
+		todofile.close();
+	}
+
+	Fl_Window *window = dynamic_cast<Fl_Window*>(w);
+	window->hide();
+}
+
+int main(int argc, char *argv[])
+{
+	Fl_Window *window = new Fl_Window(480, 640);
+	
+	Fl_Button *add_button = new Fl_Button(0, 0, 480, 25, "Add Entry");
+
+	Fl_Browser *list_view = g_list_view = new Fl_Hold_Browser(0, 25, 480, 590);
+
+	Fl_Button *edit_button = g_edit_button = new Fl_Button(0, 615, 240, 25, "Edit Entry");
+
+	Fl_Button *delete_button = g_delete_button = new Fl_Button(240, 615, 240, 25, "Delete Entry");
+
+	add_button->callback(add_cb);
+	edit_button->callback(edit_cb);
+	delete_button->callback(delete_cb);
+	window->callback(window_cb);
+
+	edit_button->deactivate();
+	delete_button->deactivate();
+
+	{
+		std::string line;
+		std::ifstream todofile(std::string(getenv("HOME"))+"/.todolist");
+		if (todofile.is_open()) {
+			while (getline(todofile, line)) {
+				g_list_view->add(line.c_str());
+			}
+			todofile.close();
+		}
+	}
+
+	list_view->callback(list_view_cb);
+	/* The following works, but it cannot capture variables, ... */
+	/*list_view->callback(
+			[](Fl_Widget*, void*) {
+				if (g_list_view->value()) {
+					g_edit_button->activate();
+					g_delete_button->activate();
+				} else {
+					g_edit_button->deactivate();
+					g_delete_button->deactivate();
+				}
+			});*/
+
+	window->resizable(list_view);
+
+	window->end();
+
+	window->show(argc, argv);
+	
+	return Fl::run();
+}
